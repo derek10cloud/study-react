eb29c6d635429146c281f1b98a8eabb9fd7fb90d
derek10cloud <derek10cloud@gmail.com>
2022-04-19 20:07:50 +0900

    add

---
+++ chapter/08.hooks/src/App.js
@@ -0,0 +1,10 @@
+import Info from "./Info";
+import Counter from "./Counter";
+import Average from "./Average";
+import RefSample from "./RefSample";
+
+const App = () => {
+  return <RefSample />;
+};
+
+export default App;
